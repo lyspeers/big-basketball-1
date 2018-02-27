cb7d83718c146e6eed346fe746cb13591c98dea1
d-allison <contact@danielallison.net>
2018-02-27 08:48:31 -0500

    Added general classes

---
+++ src/com/company/Bracket.java
@@ -0,0 +1,7 @@
+package com.company;
+
+/**
+ * Created by da426 on 2/27/18.
+ */
+public class Bracket {
+}
---
+++ src/com/company/Team.java
@@ -0,0 +1,9 @@
+package com.company;
+
+/**
+ * Created by da426 on 2/27/18.
+ */
+public class Team {
+    String teamName;
+    double winningWeight;
+}
